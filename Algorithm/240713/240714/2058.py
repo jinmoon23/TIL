5c241a623cb8e66152972e70dec30a671be090e5
jinmoon23 <wlsansdlek1@me.com>
2024-07-14 12:48:21 +0900

    자릿수더하기 문제 해결

---
+++ Algorithm/240713/240714/2058.py
@@ -0,0 +1,11 @@
+N = input()
+list = []
+for x in N:
+    list.append(int(x))
+# a = list[0]
+# b = list[1]
+# c = list[2]
+# d = list[3]
+# solution = a + b + c + d
+solution = sum(list)
+print(solution)
