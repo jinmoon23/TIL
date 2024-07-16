38c4d6a4982564135e5de1b8789356fff1bc2c68
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2024-07-16 13:29:04 +0000

    tesdt

--- Algorithm/240716(2072, 홀수만더하기)/test.py
+++ Algorithm/240716(2072, 홀수만더하기)/test.py
@@ -1,1 +1,9 @@
+import random
+
 testcase_number = int(input())
+testcase = {}
+random_numbers = []
+
+for i in range(testcase_number):
+    testcase[i] = random.randint(0,10000)
+    print testcase
