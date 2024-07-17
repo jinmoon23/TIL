47b965e6ed854ca72afb5fecd5648164619a78d6
jinmoon23 <wlsansdlek1@me.com>
2024-07-17 12:24:13 +0900

    어렵네 ㅠㅠ

--- Algorithm/240716(2072, 홀수만더하기)/test.py
+++ Algorithm/240716(2072, 홀수만더하기)/test.py
@@ -2,9 +2,12 @@ import random
 
 testcase_number = 3
 testcase = {}
-random_numbers = []
+random_list = []
 
-for i in range(testcase_number):
-    testcase[i] = random.randint(0,10000)
+for i in range(10):    
+    random_list.append(random.randint(0,10000))
+
+for j in range(testcase_number):
+    testcase[j] = random_list
     
 print(testcase)
