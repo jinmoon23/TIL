68c67ca26957d486be6c79da2c03555025c425e2
jinmoon23 <wlsansdlek1@me.com>
2024-10-04 21:33:19 +0900

    저녁 수정

--- django/day_11_pjt/pjt/README.md
+++ django/day_11_pjt/pjt/README.md
@@ -4,10 +4,39 @@
 1. keyword.html
 2. keyword/pk.html
 3. keyword/crawling.html
+4. keyword/crawling_histogram.html
 
 어려웠던 점
 1. crawling 진행 시 저장하려는 정보가 이미 테이블에 존재할 경우에 대한 분기처리가 어려웠다. 결국 해결하지 못함..
+- 이 부분 추가적으로 작업해 완성하였음!
+```py
+# 기존의 코드
+trend, created_trend = Trend.objects.get_or_create(name=name,result=result,search_period=search_period)
+# 변경된 후의 코드
+trend, created_trend = Trend.objects.get_or_create(name=name, defaults={
+    'result': result, 'search_period': search_period
+    })
+
+# get_or_create 메서드의 defaults 인자의 역할을 확실히 파악할 수 있었다.
+# 오직 name만을 고려 요인으로 두고 나머지는 중복검사를 실시하지 않는것!
+# 이 코드 변화로 인해 분기처리를 정상적으로 진행할 수 있다.
+```
 2. crawling 완료 후 원하는 정수값(총 검색횟수)으로 변환하는 과정이 힘들었다. 이건 성공!
+3. 키워드 저장하는 부분에서 동일한 키워드 네임으로 저장되는 상황이 발생했음. 그런데 `forms.py`를 활용해 `ModelForm` 형태로 유저 입력값을 처리하기 때문에 어떻게 그 키워드 네임에 접근해야할지 난감했다.
+   - 이는 form의 유효성검사와 cleaned_data 및 get()을 활용해 해결했다.
+```py
+form = KeywordForm(request.POST)
+    if form.is_valid():
+        # 유효성 검사를 완료한 경우에만 cleaned_data로 접근이 가능하고 이후 get() 메서드로 form 내부의 속성인 키값인 name에 접근할 수 있었다.
+        name = form.cleaned_data.get('name')
+        # 여기서 get이 아닌 filter를 사용하는 이유는 해당하는 name 필드가 애초에 존재하지 않았던 경우에도 에러가 발생하지 않도록 하기 위함이다. 
+        if not Keyword.objects.filter(name=name).exists():
+            form.save()
+            return redirect('trends:keyword')
+        else:
+            form.add_error('name','이미 존재하는 키워드입니다.')
+```
 
 느낀점
 1. crawling의 핵심적인 데이터 받아오기를 성공해서 뿌듯했지만 만족스럽게 분기처리를 하지 못한것이 아쉽다. 디테일한 부분을 더 공부하도록 해야겠다 ^_^
+2. 확실히 시각화 부분(matplotlib)은 반복되는 파트가 많다. 익숙해지는것도 물론 중요하겠지만 영리하게 찾아보고 적용할 수 있어야겠다.
--- django/day_11_pjt/pjt/trends/views.py
+++ django/day_11_pjt/pjt/trends/views.py
@@ -4,7 +4,12 @@ from .forms import KeywordForm
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import matplotlib.pyplot as plt
+import matplotlib
+import base64
+from io import BytesIO
 
+matplotlib.use('Agg')
 def get_google_data(keyword):
     url = f"https://www.google.com/search?q={keyword}"
     # Chrome 옵션 설정
@@ -30,9 +35,14 @@ def keyword(request):
     keywords = Keyword.objects.all()
     if request.method == 'POST':
         form = KeywordForm(request.POST)
-        form.save()
-        # keyword, created = Keyword.objects.get_or_create(form)
-        return redirect('trends:keyword')
+        if form.is_valid():
+            # 동일한 키워드로 중복 저장되지 않도록 하는 로직
+            name = form.cleaned_data.get('name')
+            if not Keyword.objects.filter(name=name).exists():
+                form.save()
+                return redirect('trends:keyword')
+            else:
+                form.add_error('name','이미 존재하는 키워드입니다.')
     else:
         form = KeywordForm()
     context = {
@@ -47,12 +57,12 @@ def keyword_detail(request,pk):
     keyword.delete()
     return redirect('trends:keyword')
 
-# 크롤링 수행 및 crawling.html로 리다이렉션
+# 크롤링 수행 및 crawling.html 렌더링
 def crawling(request):
     keywords = Keyword.objects.all()
     for keyword in keywords:
         # '검색결과 약 xx개' 형태의 문자열
-        dummy = get_google_data(keyword)
+        dummy = get_google_data(keyword.name)
         int_lst = []
         for char in dummy:
             if char == '(':
@@ -63,9 +73,11 @@ def crawling(request):
         result = int(''.join(int_lst))
         name = keyword.name
         search_period = 'all'
-        # 이렇게 해도 중복제거가 안된다.. 이유가 뭘까..ㅜㅜ
-        trend, created_trend = Trend.objects.get_or_create(name=name,result=result,search_period=search_period)
-        # trend = Trend(name=name,result=result,search_period=search_period)
+        trend, created_trend = Trend.objects.get_or_create(name=name, defaults={
+            'result': result,
+            'search_period': search_period
+            }
+        )
     trends = Trend.objects.all()
     context = {
         'trends':trends,
@@ -74,7 +86,38 @@ def crawling(request):
 
 # 크롤링 수행 후 수행결과 막대 그래프 생성 및 crawling_histogram.html 렌더링
 def crawling_histogram(request):
-    pass
+    # 1. 트렌드 데이터 가져오기
+    trends = Trend.objects.all()
+
+    # 2. name과 result 값 분리
+    names = [trend.name for trend in trends]
+    results = [trend.result for trend in trends]
+
+    # 3. 막대 그래프 그리기
+    plt.figure(figsize=(10,6))  # 그래프 크기 설정
+    plt.bar(names, results, label='Trends')
+
+    # 4. 그래프 레이블 설정
+    plt.xlabel('Keyword')
+    plt.ylabel('Result')
+    plt.title('Technology Trend Analysis')
+    plt.xticks(rotation=45)  # 이름이 겹치지 않도록 45도 회전
+    plt.legend(loc='upper right') # 범례 위치 설정
+
+    # plt.show
+    # 5. 비어 있는 버퍼 생성
+    buffer = BytesIO()
+    # 6. 버퍼에 그래프를 저장
+    plt.savefig(buffer,format='png')
+    # 7. 버퍼의 내용을 인코딩
+    image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8').replace('\n','')
+    buffer.close()
+    context = {
+        'chart_image': f'data:image/png;base64,{image_base64}',
+    }
+
+    return render(request,'trends/crawling_histogram.html', context)
+
 
 # 지난 1년을 기준으로 크롤링 수행 후 수행 결과 막대 그래프 생성 및 crawling_advanced.html 렌더링
 def crawling_advanced(request):
