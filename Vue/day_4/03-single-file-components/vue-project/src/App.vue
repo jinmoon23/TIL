52e5ac356481df65a505c4ccb6a7be1eeb67c988
jinmoon23 <wlsansdlek1@me.com>
2024-11-07 17:57:01 +0900

    저녁수정

---
+++ Vue/day_4/03-single-file-components/vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>App.vue</h1>
+<!-- 3. 호출 -->
+<MyComponent />
+</template>
+
+<script setup>
+// 2. App.vue import
+// import MyComponent from './components/MyComponent.vue'
+// ./components는 반복된다.
+// src가 핵심이므로 src를 위한 절대경로를 정의했음
+// 여기서 @/ == src, 자주 사용하는 약어라고 생각하면 됨
+import MyComponent from '@/components/MyComponent.vue'
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/day_4/vue_ex_4_1/vue-project/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <MyComponent/>
+</template>
+
+<script setup>
+import MyComponent from '@/components/MyComponent.vue';
+</script>
+
+<style scoped>
+
+</style>
