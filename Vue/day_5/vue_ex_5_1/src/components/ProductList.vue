0c942d5040af9ce6de8b7c1ea7fa3b60c7e93fce
jinmoon23 <wlsansdlek1@me.com>
2024-11-08 17:50:03 +0900

    저녁수정

---
+++ Vue/day_5/vue_ex_5_1/src/components/ProductList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <li v-for="product in propProducts" :key="product.id">
+        {{ product.name }} - {{ product.price }}
+      </li>
+    </div>
+</template>
+
+<script setup>
+// 2. 자식 컴포넌트에서 props 수신
+defineProps({
+    propProducts: Array
+})
+</script>
+
+<style scoped>
+
+</style>
