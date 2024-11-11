114ee39d8d0f71ea1d988c5ae880813b835c2b19
jinmoon23 <wlsansdlek1@me.com>
2024-11-11 21:50:49 +0900

    밤 수정

---
+++ Vue/day_6/vue_hw_6_4/src/views/StudentViews.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <ul>
+        <li v-for="student in students">
+          {{ student.name }} 학생 정보 보기
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+const students = ref([
+  {name:'김하나'},
+  {name:'김두리'},
+  {name:'김서이'},
+])
+</script>
+
+<style scoped>
+
+</style>
